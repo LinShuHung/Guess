ff1969d6edcacca5338640113b1f3067374c2bf5
Shu-Hung Lin <suhunlin0310@gmail.com>
2023-03-09 11:55:21 +0800

    add linear layout style(comment)(//2.LinearLayout)

--- app/src/main/java/com/suhun/guess/MainActivity.kt
+++ app/src/main/java/com/suhun/guess/MainActivity.kt
@@ -3,8 +3,11 @@ package com.suhun.guess
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.suhun.guess.databinding.ActivityMainBinding
-
+import com.suhun.guess.databinding.LinearMainBinding
+//1.ConstraintLayout
 private lateinit var binding:ActivityMainBinding
+//2.LinearLayout
+//private lateinit var binding:LinearMainBinding
 
 
 class MainActivity : AppCompatActivity() {
@@ -12,10 +15,19 @@ class MainActivity : AppCompatActivity() {
     val secretNumber = SecretNumber()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-//        setContentView(R.layout.activity_main)
+        //1.ConstraintLayout
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 //        binding.count.text = secretNumber.randomNumber.toString()
 //        binding.information.text = secretNumber.verify(88)
     }
+    //2.LinearLayout
+    /*override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+//        binding = LinearMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+//        binding.count.text = secretNumber.randomNumber.toString()
+//        binding.information.text = secretNumber.verify(88)
+    }*/
+
 }
